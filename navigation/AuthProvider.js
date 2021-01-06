b1f804b123dc04b2caba96f451fba4a50ac7d876
RukshanNuwan <39560159+RukshanNuwan@users.noreply.github.com>
2021-01-06 16:34:09 +0530

    have some error

--- navigation/AuthProvider.js
+++ navigation/AuthProvider.js
@@ -17,14 +17,14 @@ export const AuthProvider = ({ children }) => {
           try {
             await auth().createUserWithEmailAndPassword(email, password).then((rest) => {
               console.log(rest);
-              console.log("User account created & signed in!");
+              console.log("User account created & signed in");
             }).catch(error => {
               if (error.code === "auth/email-already-in-use") {
                 console.log("That email address is already in use!");
               }
 
               if (error.code === "auth/invalid-email") {
-                console.log("That email address is invalid!");
+                console.log("That email address is invalid");
               }
 
               console.error(error);
